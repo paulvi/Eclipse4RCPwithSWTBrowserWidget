1819bef4f041eee828b782dae6e884548348ae01
PaulV <paul.verest@live.com>
2013-03-30 19:27:27 +0800

    Add View with Browser

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Eclipse4RCPwithSWTBrowserWidget
 ===============================
 
 following tutorials
+
+
+http://www.vogella.com/articles/EclipseRCP/article.html#tutorial_e4wizard
+
+http://www.vogella.com/blog/2009/10/29/javascript-swt-browser-widget/
+
+docs
+
+http://www.eclipse.org/swt/widgets/
+http://help.eclipse.org/juno/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Freference%2Fapi%2Forg%2Feclipse%2Fswt%2Fbrowser%2FBrowser.html
---
+++ src/com/example/e4/rcp/browser/View.java
@@ -0,0 +1,38 @@
+package com.example.e4.rcp.browser;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+ 
+/**
+ * @author pverest
+ *
+ */
+public class View extends ViewPart {
+ 
+    public static final String ID = "de.vogella.swtbrowser.view";
+     
+    public void createPartControl(Composite parent) {
+        final Browser b = new Browser(parent, SWT.NONE);
+        b.setUrl("www.vogella.de");
+        b.addProgressListener(new ProgressListener() {
+            @Override
+            public void completed(ProgressEvent event) {
+                System.out.println("Page loaded");
+                System.out.println(b.execute("alert(\"1\");"));
+            }
+            @Override
+            public void changed(ProgressEvent event) {
+            }
+        });
+    }
+ 
+    /**
+     * Passing the focus request to the viewer's control.
+     */
+    public void setFocus() {
+    }
+}
